67dad90acb42a9130b659b34b68b88fc9f100e3e
abdelhafid <hafidhpassus@gmail.com>
2023-02-26 18:17:36 +0200

    j'ai changé la communaction entre le back-end et le front-end de json-server par flask_restfull v-2

---
+++ Connection.py
@@ -0,0 +1,154 @@
+from pymongo import MongoClient
+import pandas as pd
+import numpy as np
+
+import json
+class Connection():
+
+
+    def __init__(self):
+        login = 'Jean-claude'
+
+        password = 'azertyuiop'  # J'avais changé le mot de passe pendant mes test, c'est le nouveau mot de passe.
+        host = 'cluster0.kwhckqe.mongodb.net'
+        db = 'UserData'
+        uri = f'mongodb+srv://{login}:{password}@{host}/{db}?retryWrites=true&w=majority'
+        client = MongoClient(uri)
+        self.db = client['UserData']
+
+    def charge_data(self):
+        electricite_collection = self.db['Electricite_data_final']
+        data = electricite_collection.find({})
+
+        date = []
+        heure = []
+        consomation = []
+        for document in data:
+            try:
+                # print(document)
+                consomation.append(document['consommation'])
+                date.append(document['date'])
+                heure.append(document['heures'])
+
+            except:
+                pass
+
+        self.df = pd.DataFrame(data={'date': date, 'consommation': consomation})
+
+        self.df.date = pd.to_datetime(self.df['date'], dayfirst=True)
+        # df.set_index(df.date,inplace=True)
+
+
+    def data_by_day(self):
+        electricite_collection = self.db['Electricite_data_final']
+        data = electricite_collection.find({})
+
+        date = []
+        heure = []
+        consomation = []
+        for document in data:
+            try:
+                # print(document)
+                consomation.append(document['consommation'])
+                date.append(document['date'])
+                heure.append(document['heures'])
+
+            except:
+                pass
+
+        self.df_day =  pd.DataFrame(data={'date':date,'heure':heure , 'consommation':consomation})
+        self.df_day['heure'] = self.df_day['heure'].apply(lambda x: x + ':00')
+        self.df_day['heure'] = pd.to_datetime(self.df_day['heure'].values)
+        self.df_day['heure'] = self.df_day['heure'].dt.hour
+        self.df_day = self.df_day.groupby(by=['date', 'heure']).mean()
+
+        dic = dict()
+
+        index = self.df_day.index
+        cons = self.df_day.consommation.values
+
+        for i in range(len(index)):
+            if index[i][0] not in dic:
+                dic[index[i][0]] = dict()
+                dic[index[i][0]]['heure'] = list()
+                dic[index[i][0]]['consomation'] = list()
+
+            dic[index[i][0]]['heure'].append(str(index[i][1]))
+            dic[index[i][0]]['consomation'].append(str(cons[i]))
+
+
+        #with open('data.json', 'w') as fp:
+        #    json.dump(dic, fp)
+
+        return dic
+
+
+
+    def data_by_year(self):
+        self.df_year =  self.df.groupby(self.df.date.dt.year).mean()
+
+    def data_by_month(self,year_1,year_2):
+        self.df_month = self.df[((self.df.date > year_1) & (self.df.date < year_2))]
+
+        self.df_month = self.df_month.groupby(self.df_month.date.dt.month).mean()
+
+    def data_by_year_month(self,year,month):
+        self.df_year_month = self.df[(self.df.date.dt.year == year) & (self.df.date.dt.month == month)]
+        self.df_year_month = self.df_year_month.groupby(self.df_year_month.date.dt.day).mean()
+
+
+    def create_semester_data(self):
+        df_sem = self.df.groupby(self.df.date).mean()
+
+        spring_19 = df_sem[(df_sem.index >= '2019-04-02') & (df_sem.index <= '2019-06-20') ].sum()[0]
+        summer_19 = df_sem[(df_sem.index >= '2019-06-21') & (df_sem.index <= '2019-09-20') ].sum()[0]
+        autumn_19 = df_sem[(df_sem.index >= '2019-09-21') & (df_sem.index <= '2019-12-20') ].sum()[0]
+        winter_19 = df_sem[(df_sem.index >= '2019-12-21') & (df_sem.index <= '2019-12-31') ].sum()[0]
+
+        spring_20 = df_sem[(df_sem.index >= '2020-03-21') & (df_sem.index <= '2020-06-20')].sum()[0]
+        summer_20 = df_sem[(df_sem.index >= '2020-06-21') & (df_sem.index <= '2020-09-20')].sum()[0]
+        autumn_20 = df_sem[(df_sem.index >= '2020-09-21') & (df_sem.index <= '2020-12-20')].sum()[0]
+        winter_20 = df_sem[((df_sem.index >= '2020-12-21') & (df_sem.index <= '2020-12-31') ) |
+                           ((df_sem.index >= '2020-01-01') & (df_sem.index <= '2020-03-20'))
+                           ].sum()[0]
+
+        spring_21 = df_sem[(df_sem.index >= '2021-03-21') & (df_sem.index <= '2021-06-20')].sum()[0]
+        summer_21 = df_sem[(df_sem.index >= '2021-06-21') & (df_sem.index <= '2021-09-20')].sum()[0]
+        autumn_21 = df_sem[(df_sem.index >= '2021-09-21') & (df_sem.index <= '2021-12-20')].sum()[0]
+        winter_21 = df_sem[((df_sem.index >= '2021-12-21') & (df_sem.index <= '2021-12-31')) |
+                           ((df_sem.index >= '2021-01-01') & (df_sem.index <= '2021-03-20'))
+                           ].sum()[0]
+        spring_22 = df_sem[(df_sem.index >= '2022-03-21') & (df_sem.index <= '2022-06-20')].sum()[0]
+        summer_22 = df_sem[(df_sem.index >= '2022-06-21') & (df_sem.index <= '2022-09-20')].sum()[0]
+        autumn_22 = df_sem[(df_sem.index >= '2022-09-21') & (df_sem.index <= '2022-12-20')].sum()[0]
+        winter_22 = df_sem[((df_sem.index >= '2022-12-21') & (df_sem.index <= '2022-12-31')) |
+                           ((df_sem.index >= '2022-01-01') & (df_sem.index <= '2022-03-20'))
+                           ].sum()[0]
+
+        dic = {
+            '2019' : {'labels':  ['Printemps','Ete','Automne','Hiver'] , 'data':[spring_19,summer_19,autumn_19,winter_19]},
+            '2020': {'labels': ['Printemps', 'Ete', 'Automne', 'Hiver'],
+                     'data': [spring_20, summer_20, autumn_20, winter_20]},
+            '2021': {'labels': ['Printemps', 'Ete', 'Automne', 'Hiver'],
+                     'data': [spring_21, summer_21, autumn_21, winter_21]},
+            '2022': {'labels': ['Printemps', 'Ete', 'Automne', 'Hiver'],
+                     'data': [spring_22, summer_22, autumn_22, winter_22]}
+
+        }
+        #with open('data_semestre.json', 'w') as fp:
+        #    json.dump(dic, fp)
+
+        return dic
+    def skip_null(self, tmp,ind, rng=12):
+        cons_by_month= []
+
+        j = 0
+        for i in range(rng):
+            if i + 1 in ind:
+                cons_by_month.append(str(tmp[j]))
+                j += 1
+            else:
+                cons_by_month.append("N/A")
+
+        cons_by_month = list(cons_by_month)
+        return cons_by_month
--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask, render_template, url_for, request
 import pandas as pd
 import os
+from Connection import Connection
+import json
+import numpy as np
+from dateutil import rrule
+import datetime
+import pickle
+import statsmodels.api as sm
+from flask_restful import Resource, Api
+from flask_cors import CORS
+from flask_cors import cross_origin
+
 
 app = Flask(__name__)
+CORS(app)
+
+api = Api(app)
 
 
 #insertion de l'image
@@ -11,116 +25,91 @@ IMG_FOLDER = os.path.join('static', 'images')
 app.config['UPLOAD_FOLDER'] = IMG_FOLDER
 Flask_Logo = os.path.join(app.config['UPLOAD_FOLDER'], 'enedis3.png')
 
-conso_list =[]
 
-def uploadCsv():
-    input_file = "C:/Users/Saliou95/Desktop/Hackathon RESPONSE/Data/CDC par client/ACDC_CC_STM A 25_11_2022.csv"
-    file = open(input_file,"r")
-    lines = file.readlines()
-    dates = []
-    heures = []
-    conss = []
+Connex  = Connection()
+Connex.charge_data()
+dic_day = Connex.data_by_day()
 
-    #verifier si les consommations sont par 5 minutes ou bien par 10 minutes
-
-    line1 = lines[1].replace("\n","").split(" ")
-    date1 = line1[0]
-    heure_cons1 = line1[1].split(";")
-    heure1 = ":".join(heure_cons1[0].split(":")[:2])
-    min1 = int(heure1.split(":")[1])
-
-
-    line2 = lines[2].replace("\n","").split(" ")
-    date2 = line2[0]
-    heure_cons2 = line2[1].split(";")
-    heure2 = ":".join(heure_cons2[0].split(":")[:2])
-    min2 = int(heure1.split(":")[1])
-
-    by_10 = False
-
-    #si les deux premiers valeurs de consomation sont multiple de 10 alors le fichier peut contenir des durées de 10 minutes et de 5 minutes
-    if ((min1 % 10)== 0) and ((min2 % 10)  == 0):
-        by_10 = True
-
-
-    if by_10 :
-        for i in range(1,len(lines)):
-
-            line = lines[i].replace("\n","").split(" ")
-            date = line[0]
-            heure_cons = line[1].split(";")
-            heure = ":".join(heure_cons[0].split(":")[:2])
-            min = int(heure.split(":")[1])
-            cons = heure_cons[1]
-            cons = float(cons.replace(",","."))
-
-            if (min % 10) == 5 :
-                conss[-1] += cons
-                conss[-1] = conss[-1]/2
-            else :
-                dates.append(date)
-                heures.append(heure)
-                conss.append(cons)
-    else:
-        for i in range(1, len(lines)):
-
-            line = lines[i].replace("\n", "").split(" ")
-            date = line[0]
-            heure_cons = line[1].split(";")
-            heure = ":".join(heure_cons[0].split(":")[:2])
-            min = int(heure.split(":")[1])
-            cons = heure_cons[1]
-            cons = float(cons.replace(",", "."))
-
-
-            dates.append(date)
-            heures.append(heure)
-            conss.append(cons)
-
-    data = pd.DataFrame(data = {"date":dates,"heure":heures,"consomation":conss})
-    data["date"] = pd.to_datetime(data["date"])
-    #data["heure"] = pd.to_datetime(data["heure"],format='%H:%M')
-    data["consomation"] = data["consomation"].astype("float")
-    #format='%H:%M
-
-
-    # print(data)
-    conso_list = list(data["consomation"])
-    heures_list = list(data["heure"])
-    dates_list = list(data["date"].astype('str'))
-    
-    print(type(conso_list))
-    # finalDataset=[]
-    # finalDataset.append(conso_list, dates_list)
-    return conso_list, dates_list
-    # fileJson = data.to_json("clean_file.json")
-
+dic_ann =dict()
+dic_y_m = dict()
+dic_sem = dict()
 
 
 @app.route('/', methods=['GET', 'POST'])
+@cross_origin()
 def home():
-    conso_list, dates_list=uploadCsv()
-    req_result = False
-    if request.method == 'POST':
-        form = request.form
-        req_result = data(form)
-    return render_template('pages/home.html', datas = req_result, conso = conso_list, dates=dates_list, user_image=Flask_Logo)
 
 
-def data(form):
-    hce = request.form['hce']
-    Hparams = int(request.form['Hparams'])
-    qt_int = int(request.form['qt'])
- 
-#   qt_seconds = qt_int / 1000 
-#   rr_interval = (6000 / heart_rate) 
-#   QTc = qt_seconds / math.sqrt(rr_interval) 
-#   formated_QTc = round((QTc * 1000) * 10, 0)
-#   if (formated_QTc > 440 and sex == 'm') or (formated_QTc > 460 and sex == 'f'):
-#     prolonged = True
-#   else:
-#     prolonged = False
-    return ""
+    Connex.data_by_month('2019', '2020')
+    cons_by_month_2019 = Connex.skip_null(Connex.df_month.values.reshape(-1) , Connex.df_month.index)
+
+    Connex.data_by_month('2020', '2021')
+    cons_by_month_2020 = Connex.skip_null(Connex.df_month.values.reshape(-1) , Connex.df_month.index)
+    Connex.data_by_month('2021', '2022')
+    cons_by_month_2021 = Connex.skip_null(Connex.df_month.values.reshape(-1), Connex.df_month.index)
+    Connex.data_by_month('2022','2023')
+    cons_by_month_2022 = Connex.skip_null(Connex.df_month.values.reshape(-1) , Connex.df_month.index)
+
+    dic_ann['2019'] = {'data' :  cons_by_month_2019}
+    dic_ann['2020'] = {'data' :  cons_by_month_2020}
+    dic_ann['2021'] = {'data' :  cons_by_month_2021}
+    dic_ann['2022'] = {'data' :  cons_by_month_2022}
+
+    #with open('data_ann.json', 'w') as fp:
+    #   json.dump(dic_ann, fp)
+
+
+
+
+    month = 4
+    year = 2019
+
+    while (year < 2023) :
+        try :
+            print(month)
+            Connex.data_by_year_month(year,month)
+            rs = Connex.skip_null( list(Connex.df_year_month.values.reshape(-1)) , Connex.df_year_month.index , Connex.df_year_month.index[-1])
+            labels = []
+            for j in range(Connex.df_year_month.index[-1]):
+                labels.append(j+1)
+
+            if month < 10:
+                mth ='0'+ str(month)
+            else:
+                mth = str(month)
+            dic_y_m[str(year) + '-' + mth] = {}
+            dic_y_m[str(year) + '-' + mth]['data'] = rs
+            dic_y_m[str(year) + '-' + mth]['label'] = labels.copy()
+            print(dic_y_m[str(year)+'-'+mth])
+
+        except:
+            pass
+        month+=1
+        if month >12 :
+            month = 1
+            year +=1
+
+    #with open('data_y_m.json', 'w') as fp:
+    #    json.dump(dic_y_m, fp)
+
+
+    #prevision sur les deux prochaines jours
+    # with open('arma.pkl','rb') as f:
+    #    model =  pickle.load(f)
+
+    model = sm.load('arma_s.pkl')
+    prd = model.predict(698,699)
+
+    last_day_pr = float(dic_y_m['2022-12']['data'][-1])
+
+    jr1_prevision = int(last_day_pr + prd.values[0]*last_day_pr/100)
+    jr2_prevision = int(jr1_prevision + prd.values[1]*jr1_prevision/100)
+    global dic_sem
+    dic_sem = Connex.create_semester_data()
+    print("semmm",dic_sem)
+
+    return render_template('pages/home.html',jr1_prv=jr1_prevision,jr2_prv=jr2_prevision, user_image=Flask_Logo)
+
 
 
 @app.route('/about')
@@ -133,6 +122,34 @@ def contact():
 
 
 
+
+class by_year_month(Resource):
+    def get(self,y_m):
+        return   dic_y_m[y_m]
+
+api.add_resource(by_year_month, '/cons_y_m/<string:y_m>')
+
+class by_year(Resource):
+    def get(self,y):
+        return  dic_ann[y]
+
+api.add_resource(by_year, '/cons_y/<string:y>')
+
+class by_day(Resource):
+    def get(self,day):
+        return dic_day[day]
+
+api.add_resource(by_day, '/cons_day/<string:day>')
+
+class by_sem(Resource):
+    def get(self,sem):
+        return dic_sem[sem]
+
+api.add_resource(by_sem, '/cons_sem/<string:sem>')
+
+
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('errors/404.html', user_image=Flask_Logo), 404
@@ -140,4 +157,4 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5080)
+    app.run(debug=True)
